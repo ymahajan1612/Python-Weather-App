8d7fd0b3d9091c78a727e5df6136e27021396826
Yash Mahajan <ymahajan1612@gmail.com>
2021-12-22 15:33:31 +0000

    Now using the Openweather api to fetch the weather data for a specific city in Json format. At the moment, the user is able to enter the location in the search bar and when they click the search button, the weather for the location is printed to the console. It has not yet been formatted

--- weather.py
+++ weather.py
@@ -1,17 +1,19 @@
 import pygame
 from weather_classes import *
 import sys
+import requests
 pygame.init()
 
 #colours
 white = (255,255,255)
+api_key = '44e522bdab1e065926ef411813823305'
+display = Window(600,500,"Weather Around the World",None,"mountain_bg.jpg",None)
+window = display.create_window()
 
+search_bar = Text_box(90,10,400,80,str,100)
+search_button = Button(500,0,"",",","",white,"",100,100,"search_icon.png")
+objects = [search_bar,search_button]
 def home():
-	display = Window(600,500,"Weather Around the World",None,"mountain_bg.jpg",None)
-	window = display.create_window()
-	search_bar = Text_box(90,10,400,80,str,100)
-	#search_icon = pygame.image.load("search_icon.png")
-	search_button = Button(500,0,"",",","",white,"",100,100,"search_icon.png")
 	location = None
 	while True:
 		display.update_window(None,pygame.image.load("mountain_bg.jpg"))
@@ -25,8 +27,13 @@ def home():
 			elif event.type == pygame.MOUSEBUTTONDOWN:
 				if search_button.isClicked(pygame.mouse.get_pos()):
 					location = search_bar.get_text()
-					print(location)
+					show_weather(location)
 		pygame.display.update()
+
+def show_weather(location):
+	weather_data = requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}".format(location,api_key))
+	print(weather_data.json())
+	home()
 
 
 home()
